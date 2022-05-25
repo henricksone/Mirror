d38ef45847f7192a2812e39a57c0e1d15f88f9cc
henricksone <erik.henrickson@om.org>
2022-05-25 15:25:06 -0400

    Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-// Call Hello World
-function HelloWorld(){
-    alert("hello World");
+// Get string value from page
+// Control
+function getValues(){
+    let userString = document.getElementById("userString").value;
+    return userString;
+}
+
+// Reverse the String
+// Logic
+function reverseString(userString){
+    let revString = [];
+    // reverse using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];        
+    }
+    return revString;
+}
+
+// Dispaly the Reversed String
+// View
+function displayString(revString){
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
+}
+
+// Main
+function main(){
+    document.getElementById("alert").classList.add("invisible");
+    displayString(reverseString(getValues()));
 }
